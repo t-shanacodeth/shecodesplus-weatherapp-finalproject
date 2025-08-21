10925b9bd188ca09071881bca6de57c72222988d
Tishana <t.bascombe@outlook.com>
2025-08-21 21:44:55 +0100

    Add initial functionality to the search engine

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-console.log("hello world!");
+function searchButtonSubmit(event) {
+  event.preventDefault();
+}
+
+let weatherAppSearchForm = document.getElementById("weather-app-form");
+weatherAppSearchForm.addEventListener("submit", searchButtonSubmit);
