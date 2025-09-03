c94acd06f29c3ed2a03b0c7f5498542876268b93
Tishana <t.bascombe@outlook.com>
2025-09-03 23:55:13 +0100

    Add api integration for weather stats

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,57 @@
+function refreshWeather(response) {
+  let responseData = response.data;
+  console.log(responseData);
+
+  // Update date and time
+
+  // Update city
+
+  let locationElement = document.querySelector("#location");
+  locationElement.innerHTML = responseData.city;
+
+  // Update temperature
+  let apiTemperature = Math.round(responseData.temperature.current);
+  let currentTemperature = document.querySelector("#temperature");
+  currentTemperature.innerHTML = apiTemperature;
+
+  // Update cloudy cover
+
+  // Update weather icon
+
+  // Update precipitation
+  let apiFeelsLike = Math.round(responseData.temperature.feels_like);
+  let feelsLike = document.querySelector("#feels-like");
+  feelsLike.innerHTML = apiFeelsLike;
+
+  // Update humidity
+  let apiHumidity = Math.round(responseData.temperature.humidity);
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = apiHumidity;
+
+  // Update wind speed
+  let apiWindSpeed = Math.round(responseData.wind.speed);
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = apiWindSpeed;
+}
+
+function searchCity(city) {
+  // call API and update interface
+
+  let apiKey = "68b934o4bafcaf00b4452c44bfc46ct3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchButtonSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-input");
-  let h1Element = document.querySelector("#location");
-  h1Element.innerHTML = searchInput.value;
-  console.log(searchInput.value);
-
-  // call API
-
-  let apiKey = "68b934o4bafcaf00b4452c44bfc46ct3";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
 
   // search for the city
+  searchCity(searchInput.value);
 }
 
 let weatherAppSearchForm = document.getElementById("weather-app-form");
 weatherAppSearchForm.addEventListener("submit", handleSearchButtonSubmit);
 
-axios.get(apiUrl).then;
+searchCity("London");
