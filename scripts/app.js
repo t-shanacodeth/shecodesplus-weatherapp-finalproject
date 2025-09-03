fdc347d9a00efb85741e7c726a0470221aedf5fd
Tishana <t.bascombe@outlook.com>
2025-09-03 22:22:39 +0100

    Added search engine functionality and axios

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,20 @@
-function searchButtonSubmit(event) {
+function handleSearchButtonSubmit(event) {
   event.preventDefault();
+
+  let searchInput = document.querySelector("#search-input");
+  let h1Element = document.querySelector("#location");
+  h1Element.innerHTML = searchInput.value;
+  console.log(searchInput.value);
+
+  // call API
+
+  let apiKey = "68b934o4bafcaf00b4452c44bfc46ct3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
+
+  // search for the city
 }
 
 let weatherAppSearchForm = document.getElementById("weather-app-form");
-weatherAppSearchForm.addEventListener("submit", searchButtonSubmit);
+weatherAppSearchForm.addEventListener("submit", handleSearchButtonSubmit);
+
+axios.get(apiUrl).then;
